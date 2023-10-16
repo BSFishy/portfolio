ed0d75dcfe476f37859751bdd49946fd44ecc63b
Matt Provost <mattprovost6@gmail.com>
2023-10-16 16:22:54 -0700

    Minor changes
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- layout/Centered/Centered.tsx
+++ layout/Centered/Centered.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from 'react'
 import classNames from 'classnames'
 import styles from './Centered.module.scss'
+import { Analytics } from '@vercel/analytics/react'
 
 import 'sanitize.css'
 import 'sanitize.css/typography.css'
@@ -10,7 +11,10 @@ type Props = PropsWithChildren<{}>
 const Centered: FC<Props> = ({ children }) => (
   <>
     <html lang="en" className={styles.html}>
-      <body className={classNames(styles.body)}>{children}</body>
+      <body className={classNames(styles.body)}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   </>
 )
