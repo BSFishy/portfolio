ec2627a0422b727c625175d6d97dfb68347fba29
Matt Provost <mattprovost6@gmail.com>
2024-05-12 22:14:28 -0700

    Update to Vercel speed insights
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,12 @@
 <script>
 	import { dev, browser } from '$app/environment';
-	import { webVitals } from '$lib/vitals';
-	import { inject } from '@vercel/analytics';
 	import { page } from '$app/stores';
+  import { injectSpeedInsights } from '@vercel/speed-insights';
 
 	import 'sanitize.css';
 	import 'sanitize.css/typography.css';
 
-	inject({ mode: dev ? 'development' : 'production' });
-
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId,
-			debug: dev
-		});
-	}
+  injectSpeedInsights();
 </script>
 
 <slot />
