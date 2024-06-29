8d58cca848cfdb2e6f59923a9009aefdddc5b23b
Matt Provost <mattprovost6@gmail.com>
2024-06-29 13:47:36 -0500

    feat: add cannonical url
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import 'sanitize.css';
 	import 'sanitize.css/typography.css';
+
+	import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+	<link rel="canonical" href={'https://www.mattprovost.dev' + $page.url.pathname} />
+</svelte:head>
 
 <slot />
