d7c900640724bb36fea07b6ee831ffd25a3095a1
Matt Provost <mattprovost6@gmail.com>
2024-06-29 14:00:17 -0500

    feat: move about info back to separate page
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,63 +18,9 @@
 		<a href="https://www.linkedin.com/in/matt-provost/" target="_blank"><LinkedIn size={1.5} /></a>
 		<a href="/blog/"><Blog size={1.5} /></a>
 	</div>
-	<div>
-		<p>
-			Hi, I'm Matt. I'm a Systems Engineer at <a href="https://www.cloudflare.com/">Cloudflare</a>
-			working on the
-			<a href="https://www.cloudflare.com/zero-trust/products/access/">Access Team</a>. Previously,
-			I've worked on <a href="https://opensearch.org/">the OpenSearch Project</a> at
-			<a href="https://aws.amazon.com/">Amazon Web Services</a>.
-		</p>
-		<p>
-			I'm interested in all things tech. I started programming at the age of 10, and have been
-			fueled by passion and curiousity ever since. Exploring anything that has interested me, I've
-			delved into full-stack, mobile, embedded, games, and systems development. Landing my first job
-			as a software engineer at the age of 16 introduced me to the industry, and I've been learning
-			and honing my skills ever since.
-		</p>
-		<p>
-			Overall, I'm a very curious and innovative person. My goal is to have a substantial and
-			lasting impact on all of humanity through the design and development of software. I'm
-			constantly learning through exploring new projects and contributing to open source.
-		</p>
-	</div>
-	<h2>Experience</h2>
-	<div class="full">
-		<Experience
-			company="AWS"
-			title="Web Development Engineer"
-			from="August 2022"
-			to="April 2024"
-			location="Bellevue, WA"
-			items={[
-				"Spearheaded enhancements to OpenSearch's design system (OUI), streamlining CI/CD processes and fortifying the build system.",
-				'Pioneered a dynamic content framework for the homepage, demonstrating technical proficiency and innovative thinking.',
-				'Mentored college students in the OpenSearch Contributor Initiative (OSCI), fostering a collaborative learning environment.'
-			]}
-		/>
-		<Experience
-			company="TrainBeyond"
-			title="Software Engineer"
-			from="May 2022"
-			to="July 2022"
-			location="The Woodlands, TX"
-			items={[
-				'Engineered dynamically loaded content functionality, enabling seamless downloading and local execution of remote code.',
-				'Implemented a Lua runtime for dynamic scripting of NPC features, enhancing interactivity within the training platform.'
-			]}
-		/>
-		<Experience
-			company="WebCreek"
-			title="Software Engineer"
-			from="March 2019"
-			to="May 2022"
-			location="The Woodlands, TX"
-			items={[
-				'Revitalized the iOS app "Voyager," overhauling core libraries and functionalities in Swift for improved ergonomics.',
-				"Optimized TrainBeyond's interactive training software for iOS devices, ensuring smooth performance on resource-constrained devices."
-			]}
-		/>
+	<div class="items">
+		<a href="/about/">About me</a>
+		<a href="/blog/">Blog</a>
 	</div>
 </Centered>
 
@@ -82,11 +28,6 @@
 	h1 {
 		margin: 0;
 		font-size: calc(var(--size) * 3);
-	}
-
-	h2 {
-		margin: 0;
-		font-size: calc(var(--size) * 2);
 	}
 
 	.items {
@@ -98,9 +39,6 @@
 
 	.items > a {
 		margin: var(--size);
-	}
-
-	.full {
-		width: 100%;
+		font-size: calc(var(--size) * 1.25);
 	}
 </style>
