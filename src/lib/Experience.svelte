89478b5eb3df0e6be599e4633d72698ae5ec5f8b
Matt Provost <mattprovost6@gmail.com>
2024-06-29 13:31:03 -0500

    feat: redesign home
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

---
+++ src/lib/Experience.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	export let company: string;
+
+	export let title: string;
+
+	export let items: Array<string>;
+
+	export let from: string | undefined = undefined;
+
+	export let to: string | undefined = undefined;
+
+	export let location: string | undefined = undefined;
+</script>
+
+<div class="container">
+	<h3><b>{company}</b> - {title}</h3>
+	{#if location}
+		<span class="location">{location}</span>
+		<br />
+	{/if}
+
+	{#if from}
+		{from} -
+		{#if to}
+			{to}
+		{:else}
+			Present
+		{/if}
+		<br />
+	{/if}
+
+	<ul>
+		{#each items as item}
+			<li>{item}</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+	}
+
+	h3 {
+		margin: 0;
+		font-size: calc(var(--size) * 1.5);
+	}
+
+	.location {
+		font-size: calc(var(--size) * 1.2);
+	}
+</style>
